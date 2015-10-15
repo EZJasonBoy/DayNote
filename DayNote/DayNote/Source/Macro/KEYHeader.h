8754484dee2b94a559b9e87d10365455c8d0763e
郭兆伟 <lanou3g@lanou3gdeMac-mini-111.local>
2015-10-15 10:59:27 +0800

    完整的

---
+++ DayNote/DayNote/Source/Macro/KEYHeader.h
@@ -0,0 +1,20 @@
+//
+//  KEYHeader.h
+//  ProjectMusic
+//
+//  Created by young on 15/7/31.
+//  Copyright (c) 2015年 young. All rights reserved.
+//  这里是三方key的声明/宏定义.
+
+#ifndef Project_KEYHeader_h
+#define Project_KEYHeader_h
+
+// 友盟appKey
+#define UMAPPKEY @"53cde22c56240b55aa02368c"
+
+// leancloud appKey
+#define UMAPPKEY @"53cde22c56240b55aa02368c"
+
+//
+
+#endif
